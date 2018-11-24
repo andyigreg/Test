eb3b595e573265e89b9272bd8faa4eb2e4705a5b
Andrew Gregory <>
2018-11-24 14:00:43 +0000

    equal sides of array

---
+++ Test1/cwEqualSidesOfAnArray.fs
@@ -0,0 +1,10 @@
+﻿module cwEqualSidesOfAnArray
+
+let findEvenIndex (items : int array) = 
+    let itemsList = items |> List.ofArray
+    let rec findIndex left right index =
+      match right with
+        | x::xs -> if List.sum xs = List.sum left then index else findIndex (x::left) xs (index+1)
+        | [] -> -1
+
+    findIndex [] itemsList 0
--- Test1/cwNumberContainsDigit.fs
+++ Test1/cwNumberContainsDigit.fs
@@ -1,5 +1,5 @@
 ﻿module cwNumberContainsDigit
 
 let numbersWithDigitInside (x : int64) (d : int64) = 
-    let matches =[int64 1..x] |> Seq.filter(fun n -> n = d)
-    [Seq.length matches |> int64; Seq.sum matches |> int64; (Seq.reduce (fun x y -> x * y) matches) |> int64]
+    let matches =[int64 1..x] |> Seq.filter(fun n -> string n |> Seq.contains (string d).[0])
+    if Seq.isEmpty matches then [0L; 0L; 0L] else [Seq.length matches |> int64; Seq.sum matches |> int64; (Seq.reduce (fun x y -> x * y) matches) |> int64]
