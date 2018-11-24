2238709226b0f4dcdc618bb83e7e3d16741fdea7
Andrew Gregory <>
2018-11-24 14:11:43 +0000

    online solution

--- Test1/cwEqualSidesOfAnArray.fs
+++ Test1/cwEqualSidesOfAnArray.fs
@@ -10,3 +10,16 @@ let findEvenIndex (items : int array) =
     match itemsList with
       | [] -> 0
       | _ -> findIndex [] itemsList 0
+
+
+//nice online solution
+//let rec findIt (left:List<int>) (right:List<int>) index = 
+//    match right with 
+//    | [] when left = [] -> index
+//    | [x] when left = [] -> index
+//    | head::tail when (List.sum left) = (List.sum tail) -> index
+//    | head::tail -> findIt (head::left) (tail) (index + 1)
+//    | _ -> -1
+ 
+//let findEvenIndex (items:int array) =
+    //findIt [] (Array.toList items) 0
