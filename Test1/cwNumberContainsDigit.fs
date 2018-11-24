c53f0928f56c5fdfd36ff6c273cfbca909cee030
Andrew Gregory <>
2018-11-24 10:37:18 +0000

    numbers with digits

---
+++ Test1/cwNumberContainsDigit.fs
@@ -0,0 +1,5 @@
+﻿module cwNumberContainsDigit
+
+let numbersWithDigitInside (x : int64) (d : int64) = 
+    let matches =[int64 1..x] |> Seq.filter(fun n -> n = d)
+    [Seq.length matches |> int64; Seq.sum matches |> int64; (Seq.reduce (fun x y -> x * y) matches) |> int64]
